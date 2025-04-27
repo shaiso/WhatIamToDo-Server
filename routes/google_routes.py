6e66e7b0fdbe1c8ab7789eb586c30bf7681188a8
Артур Керопьян <157704836+shaiso@users.noreply.github.com>
2025-04-27 15:50:50 +0300

    Update google_routes.py

--- routes/google_routes.py
+++ routes/google_routes.py
@@ -8,7 +8,6 @@ from extensions import db
 
 google_oauth_bp = Blueprint('google_oauth', __name__)
 
-# Инициализируем локальный OAuth-объект (можно вынести в extensions, но оставим тут)
 oauth = OAuth()
 
 google = oauth.register(
@@ -31,7 +30,6 @@ def google_login():
 def google_callback():
     """Google возвращает пользователя сюда с параметром ?code=..."""
     token = google.authorize_access_token()
-    # authorize_access_token сам обменивает code на токены
     user_info = google.parse_id_token(token)
     # user_info содержит {"sub": "...", "email": "...", "name": "...", ...}
 
@@ -48,20 +46,16 @@ def google_callback():
     # Ищем пользователя по email
     user = User.query.filter_by(email=email).first()
     if not user:
-        # Создаём нового пользователя (поле google_id заранее добавим в модель)
         user = User(email=email, name=name, google_id=google_id)
         db.session.add(user)
         db.session.commit()
     else:
-        # Если уже есть пользователь, но нет google_id, заполним
         if not user.google_id:
             user.google_id = google_id
             db.session.commit()
 
-    # Генерируем наш JWT
     access_token = create_access_token(identity=str(user.id), expires_delta=timedelta(hours=2))
 
-    # Можно вернуть JSON или редирект на фронтенд
     return jsonify({
         "message": f"Hello, {user.name}! Google login successful.",
         "access_token": access_token
